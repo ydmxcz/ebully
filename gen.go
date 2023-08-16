60ef5282954d242a1dd511b0c9f14e5a01cfeb66
ydmxcz <edmundshelby@163.com>
2023-08-16 10:55:42 +0800

    update proto command

--- gen.go
+++ gen.go
@@ -1,3 +1,3 @@
 package ebully
 
-//go:generate protoc --proto_path=./proto --go_out=./node --go_opt=paths=source_relative --go-grpc_out=./node --go-grpc_opt=paths=source_relative ./proto/node.proto
+//go:generate protoc --proto_path=./proto --go_out=./pb/node --go_opt=paths=source_relative --go-grpc_out=./pb/node --go-grpc_opt=paths=source_relative ./proto/node.proto
